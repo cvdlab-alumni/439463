f90f1caa7091a19cf5a16f36620a89a2be637c85
Valentina <vals91giuliani@gmail.com>
2014-05-22 09:36:57 +0200

    migliorato esercizio1 e modifiche esercizio2

--- 2014-05-16/python/exercise1.py
+++ 2014-05-16/python/exercise1.py
@@ -12,283 +12,81 @@ from boolean import vertexSieve
 from sysml import *
 from architectural import *
 from myfont import *
-
-def DRAW(lar) :
-    VIEW(SKEL_1(STRUCT(MKPOLS(lar))))
+from splines import *
 
 #appartamento giuliani
-master = assemblyDiagramInit([7,9,3])([[.3,3,.3,2,.3,4,.3],[.3,3,.3,2,.3,1,.3,3,.3],[0.2,2,0.2]])
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-#VIEW(hpc)
-V,CV = master
-toRemove = [88]
+master = assemblyDiagramInit([9,9,4])([[.3,3,.3,2,.3,4,.3,2,.3],[.3,3,.3,2,.3,1,.3,3,.3],[0.2,1,1,0.2]])
+copertura = master
+
+'''rimuovo le celle per definire la struttura complessiva '''
+toRemove = [254,255,290,291,294,295,257,258,259,294,295,262,263,264,265,298,299,262,265,266,267,268,269,270,271,272,273,274,275,276,277,278,279,280,281,282,283,284,285,286,287,
+			300,301,302,303,304,305,306,307,308,309,310,311,312,313,314,315,316,317,318,319,320,321,322,323]
 master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-#DRAW(master)
 hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
+hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
 #VIEW(hpc)
 
-toremove =[93]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toremove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-#VIEW(hpc)
-
-''' Aggiungo una porta nella prima stanza '''
-toMerge = 58
-door = assemblyDiagramInit([1,3,2])([[.3],[2,1,2],[1.8,.5]])
+''' Aggiungo le porte '''
+toMerge = 77
+door = assemblyDiagramInit([1,3,2])([[.3],[2,1,2],[1.8,.3]])
+door2 = assemblyDiagramInit([1,3,2])([[.3],[2,1,2],[0.2,0.3]])
 master = diagram2cell(door,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-#VIEW(hpc)
-
-''' rimuovo la cella contenente la porta'''
-toRemove = [188]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-#VIEW(hpc)
-
-toMerge = 63
-door = assemblyDiagramInit([1,3,2])([[.3],[1,1,1],[1.8,.5]])
+master = diagram2cell(door2,master,toMerge)
+toMerge =83
 master = diagram2cell(door,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-#VIEW(hpc)
-
-toRemove = [192]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-#VIEW(hpc)
-
-'''rimuovo un muro che avevo dimenticato di togliere'''
-toRemove = [46]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-#DRAW(master)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,2)
-#VIEW(hpc)
-
-'''terza porta'''
-toMerge = 67
-door = assemblyDiagramInit([1,3,2])([[.3],[0.4,0.8,0.4],[1.8,.5]])
+master = diagram2cell(door2,master,toMerge)
+toMerge = 89
+doorpic = assemblyDiagramInit([1,3,2])([[.3],[0.5,1.5,0.5],[1.8,.3]])
+door2pic = assemblyDiagramInit([1,3,2])([[.3],[0.5,1.5,0.5],[0.2,0.3]])
+master = diagram2cell(doorpic,master,toMerge)
+master = diagram2cell(door2pic,master,toMerge)
+toMerge = 159 
+master = diagram2cell(doorpic,master,toMerge)
+master = diagram2cell(door2pic,master,toMerge)
+toMerge = 143
 master = diagram2cell(door,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-''' rimuovo la cella contenente la porta'''
-toRemove = [195]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-'''terza porta'''
-toMerge = 94
-door = assemblyDiagramInit([3,1,2])([[0.5,0.8,0.5],[.3],[1.8,.5]])
+master = diagram2cell(door2,master,toMerge)
+toMerge = 211
 master = diagram2cell(door,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
+master = diagram2cell(door2,master,toMerge)
+door2 = assemblyDiagramInit([3,1,2])([[0.5,0.8,0.5],[.3],[1.8,.3]])
+door2a = assemblyDiagramInit([3,1,2])([[0.5,0.8,0.5],[0.3],[0.2,0.3]])
+toMerge=103
+master = diagram2cell(door2,master,toMerge)
+master = diagram2cell(door2a,master,toMerge)
+toMerge=125
+master = diagram2cell(door2,master,toMerge)
+master = diagram2cell(door2a,master,toMerge)
 
-''' rimuovo la cella contenente la porta'''
-toRemove = [199]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-'''quarta porta '''
-toMerge = 117
-door = assemblyDiagramInit([1,3,2])([[0.3],[0.5,0.8,0.5],[1.8,.5]])
-master = diagram2cell(door,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-''' rimuovo la cella contenente la porta'''
-toRemove = [203]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-'''Tolgo un muro che prima non ho tolto'''
-toRemove = [134]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-''' Aggiungo una porta nella prima stanza '''
-toMerge = 105
-door = assemblyDiagramInit([1,3,2])([[.3],[2,1,2],[1.8,.5]])
-master = diagram2cell(door,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-toRemove = [206]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-'''tolgo le travi sopra e un muro rimanente'''
-toRemove = [84,89,133,46,145,144]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-'''tolgo il tetto per far visualizzare la struttura 
-toRemove = [32,31,80,81,127,128,38,37,84,85,131,132,44,43,89,88,138,137,48,47,94,93,141,142]
-strutturasenzatetto = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-#VIEW(STRUCT(MKPOLS(strutturasenzatetto)))
-hpc = cellNumbering (strutturasenzatetto,hpc)(range(len(strutturasenzatetto[1])),CYAN,1)
-#VIEW(hpc)
-'''
-
-'''realizzo la prima finestra '''
-toMerge = 28
+''' aggiungo le finestre '''
+toMerge = 168
 finestra = assemblyDiagramInit([5,1,3])([[1.5,0.9,.2,.9,1.5],[.3],[1,1.4,.3]])
 master = diagram2cell(finestra,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
 
-toRemove = [206,212]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-
-'''realizzo la seconda finestra '''
-toMerge = 123
+toMerge = 38
 finestra = assemblyDiagramInit([5,1,3])([[1.5,0.9,.2,.9,1.5],[.3],[1,1.4,.3]])
 master = diagram2cell(finestra,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
 
-toRemove = [218,224]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-'''realizzo la terza finestra '''
-toMerge = 166
-finestra2 = assemblyDiagramInit([1,5,3])([[.3],[1.5,0.9,.2,.9,1.5],[1,1.4,.3]])
-master = diagram2cell(finestra2,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-toRemove = [230,236]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-'''realizzo la quarta finestra '''
-toMerge = 95
+toMerge = 198
 finestra = assemblyDiagramInit([5,1,3])([[1.5,0.9,.2,.9,1.5],[.3],[1,1.4,.3]])
 master = diagram2cell(finestra,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
 
-toRemove = [242,248]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
+toMerge = 69
+finestra = assemblyDiagramInit([5,1,3])([[1.5,0.9,.2,.9,1.5],[.3],[1,1.4,.3]])
+master = diagram2cell(finestra,master,toMerge)
 
-'''realizzo la quinta finestra '''
-toMerge = 22
-finestra2 = assemblyDiagramInit([1,5,3])([[.3],[1.5,0.9,.2,.9,1.5],[1,1.4,.3]])
-master = diagram2cell(finestra2,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
+toMerge = 130
+finestra = assemblyDiagramInit([5,1,3])([[1.5,0.9,.2,.9,1.5],[.3],[1,1.4,.3]])
+master = diagram2cell(finestra,master,toMerge)
 
-toRemove = [254,260]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
+toMerge = 14
+finestra = assemblyDiagramInit([1,5,3])([[.3],[1.5,0.9,.2,.9,1.5],[1,1.4,.3]])
+master = diagram2cell(finestra,master,toMerge)
 
-'''realizzo la sesta finestra '''
-toMerge = 10
-finestra2 = assemblyDiagramInit([1,5,3])([[.3],[1.5,0.9,.2,.9,1.5],[1,1.4,.3]])
-master = diagram2cell(finestra2,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-toRemove = [266,272]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-
-'''porta principale '''
-toMerge = 74
-door = assemblyDiagramInit([3,1,2])([[0.5,0.8,0.5],[.3],[1.8,.5]])
-master = diagram2cell(door,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-''' rimuovo la cella contenente la porta'''
-toRemove = [276]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,0.5)
-#VIEW(hpc)
-
-'''realizzo la porta finestra verso il balcone '''
-toMerge = 144
-door = assemblyDiagramInit([1,3,2])([[0.3],[0.5,0.8,0.5],[1.8,.5]])
-master = diagram2cell(door,master,toMerge)
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,1)
-#VIEW(hpc)
-
-toRemove = [280]
-master = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-hpc = SKEL_1(STRUCT(MKPOLS(master)))
-hpc = cellNumbering (master,hpc)(range(len(master[1])),CYAN,0.5)
-#VIEW(hpc)
-
-''' creo il balcone '''
-balcone = assemblyDiagramInit([3,3,3])([[0.2,2,0.2],[0.2,3,0.2],[0.2,1,0.2]])
-balcone = larApply(t(9.9,0.5,0))(balcone)
-hpc = SKEL_1(STRUCT(MKPOLS(balcone)))
-hpc = cellNumbering(balcone,hpc)(range(len(balcone[1])),CYAN,1)
-#VIEW(hpc)
-
-toRemove = [4,5,3,13,14]
-balcone = balcone[0], [cell for k,cell in enumerate(balcone[1]) if not (k in toRemove)]
-
-
-'''tolgo il tetto per far visualizzare la struttura '''
-toRemove = [28,29,77,76,121,122,35,34,81,80,126,125,41,40,84,85,131,132,135,136,90,89,44,45]
-strutturasenzatetto = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
-#a = TEXTURE('C:\Users\Valentina\Desktop\Universita\Grafica Computazionale\muro.png')(STRUCT(MKPOLS(strutturasenzatetto)))
-#VIEW(STRUCT([a]))
-VIEW(STRUCT(MKPOLS(strutturasenzatetto)+MKPOLS(balcone)))
-hpc = cellNumbering (strutturasenzatetto,hpc)(range(len(strutturasenzatetto[1])),CYAN,0.5)
-#VIEW(hpc)
-
-
-
-
-
-
+'''elimino muri di troppo e le celle contenenti finestre e porte,lascio il tetto '''
+toRemove =[166,167,170,171,172,174,175,183,182,186,187,188,190,191,102,103,106,107,108,110,111,114,115,116,118,119,124,125,39,40,47,48,55,56,59,60,61,63,64
+			,303,304,309,377,371,401,407,386,392,416,422,321,315,316,341,347,291,292,297,315,316,321,243,244,249,255,256,261,267,268,273,327,328,333,279,280,285,356,362]
+appartamentocontetto = master[0], [cell for k,cell in enumerate(master[1]) if not (k in toRemove)]
+toRemove = [141,144,150,153,91,94,97,101,39,45,51,54,386,392]
+appartamentosenzatetto = appartamentocontetto[0], [cell for k,cell in enumerate(appartamentocontetto[1]) if not (k in toRemove)]
+VIEW(STRUCT(MKPOLS(appartamentosenzatetto)))
